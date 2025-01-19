12364b161592773c3899d77cf52ae3a090167a13
Anorme <anormeinkumsah@gmail.com>
2025-01-19 14:31:00 +0000

    Extracted registration logic into registerService and used it in registerController

---
+++ smart-brain-api/services/registerService.js
@@ -0,0 +1,31 @@
+const handleRegister = async (email, name, password, db, bcrypt) => {
+  if (!email || !name || !password) {
+    throw new Error('incorrect form submission');
+  }
+  const hash = bcrypt.hashSync(password);
+
+  return db.transaction(async (trx) => {
+    try {
+      const loginEmail = await trx('login')
+        .insert({ hash, email })
+        .returning('email');
+
+      const user = await trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0].email,
+          name,
+          joined: new Date()
+        })
+      await trx.commit();
+      res.json(user)
+    } catch (error){
+      await trx.rollback()
+      throw new Error ('Unable to register');
+    }
+  });
+};
+
+module.exports = {
+  handleRegister
+}
