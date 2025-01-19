b6de47c9fdadbd776eb1ab824232c6e89e92da39
Anorme <anormeinkumsah@gmail.com>
2025-01-19 23:41:43 +0000

    Removed debugging logs from authMiddleware

--- smart-brain-api/middleware/authMiddleware.js
+++ smart-brain-api/middleware/authMiddleware.js
@@ -2,21 +2,15 @@ const redisClient = require('../services/redisClient');
 
 const requireAuth = async (req, res, next) => {
   try {
-    console.log('Authorization middleware is active')
     const { authorization } = req.headers;
-    console.log(`Checking auth headers: ${authorization}`)
     if (!authorization) {
-      console.log('No authorization header')
       return res.status(401).json('unauthorized')
     } 
 
     const reply = await redisClient.get(authorization);
-    console.log(`Checking if authorization token is in redis: ${reply}`)
     if (!reply){
-      console.log(`Token not found`)
       return res.status(401).json('unauthorized')
     }
-    console.log('You shall pass')
     return next()
 
   } catch (err){
