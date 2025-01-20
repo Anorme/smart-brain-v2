70b5d52c68a2e5d11c56f8d7423106bba568f3d2
Anorme <anormeinkumsah@gmail.com>
2025-01-20 01:09:25 +0000

    refactored signinController to use async/await

--- smart-brain-api/controllers/signinController.js
+++ smart-brain-api/controllers/signinController.js
@@ -6,18 +6,37 @@ const { handleSignin } = require('../services/signinService')
 const signin = async (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   
-  const user = await handleSignin(email, password, db, bcrypt);
-  return user;
+  try {
+    const user = await handleSignin(email, password, db, bcrypt);
+    return user;
+
+  } catch(err){
+    throw new Error('Unable to sign in');
+  }
+  
 }
 
-const signinAuthentication = (db, bcrypt) => (req, res) => {
+const signinAuthentication = (db, bcrypt) => async (req, res) => {
   const { authorization } = req.headers;
-  return authorization ? getAuthTokenId(req,res) : signin(db, bcrypt, req, res)
-    .then(data => {
-      return data.id && data.email ? createSessions(data) : Promise.reject(data)
-    })
-    .then(session => res.json(session))
-    .catch(err => res.status(400).json(err));
+  const { email, password } = req.body;
+
+  try {
+    if (authorization){
+      return getAuthTokenId(req,res)
+    } else {
+      const user = await handleSignin(email, password, db, bcrypt)
+
+      if(user && user.id && user.email){
+        const session = await createSessions(user)
+        return res.json(session);
+      } else {
+        throw new Error ('Invalid user data')
+      }
+    } 
+  } catch (err){
+    err => res.status(400).json(err.message);
+  }
+
 };
 
 module.exports = {
--- smart-brain-api/services/signinService.js
+++ smart-brain-api/services/signinService.js
@@ -4,17 +4,27 @@ const handleSignin = async (email, password, db, bcrypt) => {
   }
 
   try {
-    const loginEmail = await db.select('email', 'hash').from('login').where('email', '=', email);
-    const isValid = bcrypt.compareSync(password, loginEmail[0].hash)
+    const loginData = await db.select('email', 'hash').from('login').where('email', '=', email);
+
+    if (loginData.length === 0) {
+      throw new Error ('Invalid email');
+    }
+
+    const isValid = bcrypt.compareSync(password, loginData[0].hash)
 
     if(isValid) {
       const user = await db.select('*').from('login').where('email', '=', email);
-      return user[0]
+
+      if (user.length === 0) {
+        throw new Error ('User not found');
+      }
+      return user[0];
     } else {
-      throw new Error ('Incorrect credentials')
+      throw new Error ('Incorrect credentials');
     }
 
   } catch(err){
+    console.err('Error during sign in:', err.message);
     throw new Error('Unable to sign in')
   }
 }
