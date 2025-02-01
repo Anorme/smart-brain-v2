790daf8f8383f6176da06c11363e0fda4089cd61
Anorme <anormeinkumsah@gmail.com>
2025-02-01 12:47:44 +0000

    configured a serverless function

---
+++ smart-brain-api/ranks/handler.js
@@ -0,0 +1,23 @@
+const emojis = [
+  "🌱", "🔍", "🛠️", "🗺️", "🔥",
+  "💡", "🏆", "🥇", "🐉", "🌟"
+]
+
+
+exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length-1 : rank]
+
+  const  response = await {
+    statusCode: 200,
+    headers:{
+      'Access-Control-Allow-Origin':"*"
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v4.0! Your function executed successfully!',
+      input: rankEmoji,
+    })
+  };
+
+  return response;
+};
