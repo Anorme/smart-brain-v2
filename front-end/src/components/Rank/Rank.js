90684dbaf0282533c5fdaff897f0e351154b462f
Anorme <anormeinkumsah@gmail.com>
2025-02-01 13:42:11 +0000

    refactored Dockerfile for better code readability and effeciency

--- front-end/src/components/Rank/Rank.js
+++ front-end/src/components/Rank/Rank.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState('');
+  useEffect(() => {
+    const generateEmoji = async () => {
+      try {
+        const response = await fetch(`https://1fl19h066b.execute-api.us-east-1.amazonaws.com/rank?rank=${entries}`);
+        const result = await response.json();
+        setEmoji(result.input)
+      } catch (err) {
+        console.error('Error fetching emoji', err)
+      }
+    }
+    generateEmoji()
+  }, [entries])
+  
   return (
     <div>
       <div className='white f3'>
-        {`${name}, your current entry count is...`}
+        {`${emoji} ${name}, your current entry count is...`}
       </div>
       <div className='white f1'>
         {entries}
